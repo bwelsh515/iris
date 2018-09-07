b0741b120601a7b74184d61e131e7c00d5956873
Brian Welsh <bwelsh@syr.edu>
2018-09-07 14:41:31 -0400

    Updated ReadMe

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,18 +1,6 @@
 // api.jsx
 
 import axios from 'axios';
-// const BASE_URL = 'http://localhost:8080';
-
-// function getUserEntryData() {
-//   const url = `${BASE_URL}/api/user/entry`;
-//   return axios.get(url).then(response => response.data);
-// }
-
-// function RegisterUser() {
-//   const url = `${BASE_URL}/api/user/register`;
-// }
-
-// export { getUserEntryData };
 
 // GET - Current user from Express Sessions
 const getUser = (stateCallback) => {
